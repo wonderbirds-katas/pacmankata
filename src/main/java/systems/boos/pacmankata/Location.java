75d7b3e024598a9bc2c7de611708babe3b86d327
Stefan Boos <kontakt@boos.systems>
2023-06-02 05:53:35 +0200

    refactor: extract class Location from MoveUp

---
+++ src/main/java/systems/boos/pacmankata/Location.java
@@ -0,0 +1,4 @@
+package systems.boos.pacmankata;
+
+public record Location(int column, int row) {
+}
--- src/main/java/systems/boos/pacmankata/MoveUp.java
+++ src/main/java/systems/boos/pacmankata/MoveUp.java
@@ -2,8 +2,8 @@ package systems.boos.pacmankata;
 
 public class MoveUp {
     private boolean hasConsumedDot = true;
-    private int nextRow;
     private GameState state;
+    private Location targetLocation;
 
     public boolean hasConsumedDot() {
         return hasConsumedDot;
@@ -17,17 +17,20 @@ public class MoveUp {
     }
 
     private void calculateTargetLocation() {
-        nextRow = state.getPacManRow() - 1;
+        var nextRow = state.getPacManRow() - 1;
         if (nextRow < 1) {
             nextRow = state.getRows();
         }
+        targetLocation = new Location(state.getPacManColumn(), nextRow);
     }
 
     private boolean isDotAtTargetLocation() {
+        var nextRow = targetLocation.row();
         return Symbols.DOT == state.getSymbol(state.getPacManColumn(), nextRow);
     }
 
     private void movePacManToTargetLocation() {
+        var nextRow = targetLocation.row();
         state.setSymbol(Symbols.EMPTY_SPACE, state.getPacManColumn(), state.getPacManRow());
         state.setPacManRow(nextRow);
         state.setSymbol(Symbols.PACMAN_UP, state.getPacManColumn(), state.getPacManRow());
